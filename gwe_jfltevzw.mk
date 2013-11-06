605a973727130d76d5ecb714960e2bfcc1510ac3
Derek <drod2169@gmail.com>
2013-11-06 08:21:25 -0500

    Update fingerprint

--- gwe_jfltevzw.mk
+++ gwe_jfltevzw.mk
@@ -4,7 +4,11 @@ $(call inherit-product, vendor/gwe/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/gwe/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltevzw \
+  TARGET_DEVICE=jfltevzw \
+  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMJ7:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevzw-user 4.3 JSS15J I545VRUEMJ7 release-keys"
 
 PRODUCT_NAME := gwe_jfltevzw
 PRODUCT_DEVICE := jfltevzw
